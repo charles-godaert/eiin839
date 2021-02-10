1c27d7f9ca82e90d57ed9c4a31764caf7b2f5e3a
Charles Godaert <charles.godaert@etu.univ-amu.fr>
2021-02-10 16:14:48 +0100

    TD2 - Q1

---
+++ TD2/HttpListener/BasicServerHttpListener/Headers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicServerHTTPlistener
+{
+    public class Headers
+    {
+        private NameValueCollection headers;
+
+        public Headers(HttpListenerRequest HttpListenerRequest)
+        {
+            headers = HttpListenerRequest.Headers;
+        }
+
+        public String PrintHeaders()
+        {
+            StringBuilder builder = new StringBuilder();
+
+            for(int i = 0; i < headers.Count; i++)
+            {
+                builder.Append(headers.GetKey(i));
+                builder.Append(" : ");
+                builder.Append(headers.Get(i));
+                builder.AppendLine();
+            }
+
+            Console.WriteLine(builder.ToString());
+            return builder.ToString();
+        }
+    }
+}
