8eb625071015a75893f90521b8f9998431b9d1bd
Charles Godaert <charles.godaert@etu.univ-amu.fr>
2021-02-24 16:02:05 +0100

    Beautify exo 1

--- TD3/TD3-RestApi/TD3-RestApi/Program.cs
+++ TD3/TD3-RestApi/TD3-RestApi/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace TD3_RestApi
 {
@@ -12,25 +14,34 @@ namespace TD3_RestApi
         // HttpClient is intended to be instantiated once per application, rather than per-use. See Remarks.
         static readonly HttpClient client = new HttpClient();
 
-        static async Task Main()
+        private static Task<string> truc;
+
+        public async static Task Main()
         {
             // Call asynchronous network methods in a try/catch block to handle exceptions.
             try
             {
-                HttpResponseMessage response = await client.GetAsync("https://api.jcdecaux.com/vls/v3/contracts?apiKey=2dae8548640ca7455930eb98ca0508a102ac8c1a");
-                response.EnsureSuccessStatusCode();
-                string responseBody = await response.Content.ReadAsStringAsync();
-                Console.WriteLine(responseBody);
-
-
-
-                Console.ReadKey();
+                truc = getAllContractsAsync();
             }
             catch (HttpRequestException e)
             {
                 Console.WriteLine("\nException Caught!");
                 Console.WriteLine("Message :{0} ", e.Message);
             }
+            Console.WriteLine(truc.Result.ToString());
+            Console.ReadKey();
         }
+
+        public static async Task<string> getAllContractsAsync() {
+            return await httpRequestAsync("https://api.jcdecaux.com/vls/v3/contracts?apiKey=2dae8548640ca7455930eb98ca0508a102ac8c1a");
+        }
+
+        public static async Task<string> httpRequestAsync(String request)
+        {
+            HttpResponseMessage response = await client.GetAsync(request);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+
     }
 }
