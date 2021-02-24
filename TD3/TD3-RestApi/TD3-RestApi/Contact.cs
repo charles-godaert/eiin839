315420272e43cfaae26a7823ee081e961be041d1
Charles Godaert <charles.godaert@etu.univ-amu.fr>
2021-02-24 16:37:40 +0100

    Parse JSON

---
+++ TD3/TD3-RestApi/TD3-RestApi/Contact.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TD3_RestApi
+{
+    public class Contact
+    {
+        public String name { get; set; }
+        public String commercial_name { get; set; }
+        public String cities { get; set; }
+        public String country_code { get; set; }
+    }
+}
--- TD3/TD3-RestApi/TD3-RestApi/Program.cs
+++ TD3/TD3-RestApi/TD3-RestApi/Program.cs
@@ -6,29 +6,31 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using Newtonsoft.Json;
 
 namespace TD3_RestApi
 {
     class Program
     {
-        // HttpClient is intended to be instantiated once per application, rather than per-use. See Remarks.
         static readonly HttpClient client = new HttpClient();
 
-        private static Task<string> res;
+        private static string res;
 
         public async static Task Main()
         {
-            // Call asynchronous network methods in a try/catch block to handle exceptions.
             try
             {
-                res = getAllContractsAsync();
+                res = getAllContractsAsync().Result;
+                parseJson(res);     
             }
             catch (HttpRequestException e)
             {
                 Console.WriteLine("\nException Caught!");
                 Console.WriteLine("Message :{0} ", e.Message);
             }
-            Console.WriteLine(res.Result.ToString());
+
+            //Console.WriteLine(res);
+            
             Console.ReadKey();
         }
 
@@ -41,6 +43,15 @@ namespace TD3_RestApi
             HttpResponseMessage response = await client.GetAsync(request);
             response.EnsureSuccessStatusCode();
             return await response.Content.ReadAsStringAsync();
+        }
+
+        public static void parseJson(String json)
+        {
+            dynamic parsed = JsonConvert.DeserializeObject(res);
+            foreach (var item in parsed)
+            {
+                Console.WriteLine($"- {item.name}");
+            }
         }
 
     }
