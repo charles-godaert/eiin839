b0584eafd76434eaf907460870ac1c6bbd23eafc
Charles Godaert <charles.godaert@etu.univ-amu.fr>
2021-02-24 16:53:36 +0100

    Others questions...

--- TD3/TD3-RestApi/TD3-RestApi/Program.cs
+++ TD3/TD3-RestApi/TD3-RestApi/Program.cs
@@ -14,28 +14,52 @@ namespace TD3_RestApi
     {
         static readonly HttpClient client = new HttpClient();
 
-        private static string res;
+        private static string apiKey = "2dae8548640ca7455930eb98ca0508a102ac8c1a";
 
         public async static Task Main()
         {
             try
             {
-                res = getAllContractsAsync().Result;
-                parseJson(res);     
+                String contracts  = getAllContractsAsync().Result;
+                parseArrayJson(contracts);
+
+                String stations = getStationsFromContract("marseille").Result;
+                parseArrayJson(stations);
+
+                String station = getStationNews("9087", "marseille").Result;
+                parseJson(station);
+
+                // JCDecaux API bug
+                //String bikes = getBikesParksFromContract("marseille").Result;
+                //parseArrayJson(bikes);
+
             }
             catch (HttpRequestException e)
             {
                 Console.WriteLine("\nException Caught!");
                 Console.WriteLine("Message :{0} ", e.Message);
             }
-
-            //Console.WriteLine(res);
             
             Console.ReadKey();
         }
 
         public static async Task<string> getAllContractsAsync() {
-            return await httpRequestAsync("https://api.jcdecaux.com/vls/v3/contracts?apiKey=2dae8548640ca7455930eb98ca0508a102ac8c1a");
+            return await httpRequestAsync("https://api.jcdecaux.com/vls/v3/contracts?apiKey=" + apiKey);
+        }
+
+        public static async Task<string> getStationsFromContract(String contract)
+        {
+            return await httpRequestAsync("https://api.jcdecaux.com/vls/v1/stations?contract=" + contract + "&apiKey=" + apiKey);
+        }
+
+        public static async Task<string> getStationNews(String stationNumber, String contract)
+        {
+            return await httpRequestAsync("https://api.jcdecaux.com/vls/v3/stations/" + "9087" + "?contract=" + contract + "&apiKey=" + apiKey);
+        }
+
+        public static async Task<string> getBikesParksFromContract(String contract)
+        {
+            return await httpRequestAsync("https://api.jcdecaux.com/parking/v1/contracts/" + contract + "/parks?apiKey=" + apiKey);
         }
 
         public static async Task<string> httpRequestAsync(String request)
@@ -45,13 +69,21 @@ namespace TD3_RestApi
             return await response.Content.ReadAsStringAsync();
         }
 
-        public static void parseJson(String json)
+        public static void parseArrayJson(String json)
         {
-            dynamic parsed = JsonConvert.DeserializeObject(res);
+            dynamic parsed = JsonConvert.DeserializeObject(json);
             foreach (var item in parsed)
             {
                 Console.WriteLine($"- {item.name}");
             }
+            Console.WriteLine($"-------------------------------");
+        }
+
+        public static void parseJson(String json)
+        {
+            dynamic parsed = JsonConvert.DeserializeObject(json);
+            Console.WriteLine($"- {parsed.name}");
+            Console.WriteLine($"-------------------------------");
         }
 
     }
