226933202f1aa9d2f6877734a6b528d00062b53b
Charles Godaert <charles.godaert@etu.univ-amu.fr>
2021-03-10 15:14:02 +0100

    TD4 - Call SOAP web service Calculator

---
+++ TD4/TD4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TD4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator.CalculatorSoap calculator = new Calculator.CalculatorSoapClient();
+            Console.WriteLine(calculator.Add(11, 11));
+            Console.ReadLine();
+        }
+    }
+}
